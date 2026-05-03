8990a0354f95507e74b6c23e1dbaf4880ad03025
Rollin Gilman <rdgilman@my.canyons.edu>
2026-05-02 18:44:01 -0700

    a67 Hanoi Tower

--- main.hpp
+++ main.hpp
@@ -5,5 +5,15 @@ int hanoi(int n, char from, char to, char aux);
 
 int hanoi(int n, char from, char to, char aux)
 {
-    // TODO
+    static int count = 0;
+
+    if (n == 1)
+    {
+        count = 1;
+        return count;
+    }
+
+    count = hanoi(n - 1, from, aux, to) + 1 + hanoi(n - 1, aux, to, from);
+
+    return count;
 }
